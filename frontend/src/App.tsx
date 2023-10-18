701b09e841270ad4eb62adaa6d66f417b6622520
hou <tang_j-hou@hotmail.com>
2023-10-18 23:11:38 +0800

    updates on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./components/Login";
 
 function App() {
   const [showLogin, setShowLogin] = useState(false);
@@ -26,6 +27,9 @@ function App() {
       >
         Register
       </button>
+      {showLogin && (
+        <Login showLogin={setShowLogin} setShowLogin={setShowLogin}></Login>
+      )}
     </>
   );
 }
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,26 +1,93 @@
 import React, { useContext, useState } from "react";
+import ReactDOM from "react-dom";
 import useFetch from "../custom_hooks/useFetch";
 // import AuthContext from "../context/auth";
-import { useNavigate } from "react-router-dom";
+import styles from "./Modal.module.css";
 
 const fetchData = useFetch();
 
-const Login = () => {
+const Login = (props: any) => {
   // const auth = useContext(AuthContext);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const authRoot = document.querySelector<HTMLDivElement>("#modal-root");
-  const navigate = useNavigate();
+  const authRoot = document.querySelector<HTMLDivElement>("#modal-root")!;
 
-  const loginClick = async() => {
-    const res = await fetchData("/login", "GET", {username, password});
+  const loginClick = async () => {
+    const res = await fetchData("/login", "GET", { username, password });
     if (res.ok) {
-
+      // do something
     }
-  }
+  };
 
-  return <></>;
+  return (
+    <>
+      {ReactDOM.createPortal(
+        <div className={styles.backdrop}>
+          <div className={styles.modal}>
+            <button
+              className={styles.closeButton}
+              onClick={() => {
+                props.setShowLogin(false);
+              }}
+            >
+              <img
+                src="../../assets/close.png
+              "
+                className={styles.close}
+              />
+            </button>
+            <div className={styles.loginDetails}>
+              <div>
+                <div>Username:</div>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setUsername(e.target.value);
+                    setError("");
+                  }}
+                ></input>
+                {error ? (
+                  <p style={{ color: "red", margin: "0" }}>{error}</p>
+                ) : (
+                  <div style={{ height: "36px", margin: "0" }}></div>
+                )}
+              </div>
+              <div>
+                <div>Password:</div>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                    setError("");
+                  }}
+                ></input>
+              </div>
+
+              <button
+                onClick={() => {
+                  loginClick();
+                }}
+              >
+                Log In
+              </button>
+              <br />
+
+              <button
+                onClick={() => {
+                  props.setShowLogin(false);
+                  props.setShowRegister(true);
+                }}
+              >
+                Register
+              </button>
+            </div>
+          </div>
+        </div>,
+        authRoot
+      )}
+    </>
+  );
 };
 
 export default Login;
